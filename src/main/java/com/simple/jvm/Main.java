fc4ad1b991342cb4dcdba91ed4db804eb79581dc
mazhibin <mazhibinmail@163.com>
2019-11-22 12:01:46 +0800

    first commit

---
+++ src/main/java/com/simple/jvm/Main.java
@@ -0,0 +1,11 @@
+package com.simple.jvm;
+
+/**
+ * Author : 马志彬（120375）
+ * Date   : 2019/11/22 0022.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/com/simple/jvm/MainTest.java
@@ -0,0 +1,11 @@
+package com.simple.jvm;
+
+import static org.junit.Assert.*;
+
+/**
+ * Author : 马志彬（120375）
+ * Date   : 2019/11/22 0022.
+ */
+public class MainTest {
+
+}
